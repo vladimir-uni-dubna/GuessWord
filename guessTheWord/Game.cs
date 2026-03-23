0bc7fdcc2d40f5534f83e4fd41533b53b8d441c5
Vladimir <vladimir0kedu@gmail.com>
2026-03-23 12:12:47 +0300

    init

---
+++ guessTheWord/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guessTheWord
+{
+    internal class Game
+    {
+
+        string[] wordStorage = { "зима", "три", "источник", "пенал" };
+
+        public Game()
+        {
+            Random random = new Random();
+            while (true)
+            {
+
+                int i = random.Next(wordStorage.Length);
+                
+                string wordText = wordStorage[i];
+
+                Word word = new Word(wordText);
+
+                Run run = new Run(word);
+            }
+        }
+    }
+}
